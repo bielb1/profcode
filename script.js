2c69581baee6a4cdaecd5070cb89cd0f99f75e28
bielb1 <163067015+bielb1@users.noreply.github.com>
2024-09-07 12:06:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector('.wrapper')
+const pergunta = document.querySelector('.pergunta')
+const btnSim = document.querySelector('.btn-sim')
+const btnNao = document.querySelector('.btn-nao')
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const btnNaoRect = btnNao.getBoundingClientRect();
+
+btnSim.addEventListener('click', () => {
+  pergunta.innerHTML = 'Tô Chegando ';
+});
+
+btnNao.addEventListener('mouseover', () => {
+  const i = Math.floor(Math.random() * (wrapperRect.width - btnNaoRect.width)) +3;
+  const j = Math.floor(Math.random() * (wrapperRect.height - btnNaoRect.height)) +3;
+
+  btnNao.style.left = i + 'px';
+  btnNao.style.top = j + 'px';
+});
